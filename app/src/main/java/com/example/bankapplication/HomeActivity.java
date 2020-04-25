55417e99ae3dc11e9312ae09097565c9f23fe3ee
KakkuCake <k.orelma7@gmail.com>
2020-04-25 19:49:15 +0300

    Tässä koodit.

--- app/src/main/java/com/example/bankapplication/HomeActivity.java
+++ app/src/main/java/com/example/bankapplication/HomeActivity.java
@@ -1,67 +1,81 @@
-
 package com.example.bankapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
 
-import java.util.HashMap;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity {
-
-
-    String mName, mEmail;
-    private TextView name, email;
-    private Button button_logout;
-    private Button button_edit_profile;
-    SessionManager sessionManager;
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        sessionManager = new SessionManager(this);
-        sessionManager.checkLogin();
 
-        name = (TextView) findViewById(R.id.name);
-        email = (TextView) findViewById(R.id.email);
-        button_logout = (Button) findViewById(R.id.button_logout);
-        button_edit_profile = (Button) findViewById(R.id.button_edit_profile);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        HashMap<String, String> user = sessionManager.getUserDetail();
-        String mName = user.get(sessionManager.FIRST_NAME);
-        String mEmail = user.get(sessionManager.EMAIL);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
-        name.setText(mName);
-        email.setText(mEmail);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        button_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sessionManager.logout();
-            }
-        });
-
-        button_edit_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivityEditProfile();
-            }
-        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.navigation_home);
+        }
     }
 
-    // This method gets the user details
-    private void startActivityEditProfile() {
-        Intent intent = new Intent(HomeActivity.this, EditProfileActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.navigation_bank_actions:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new BankActionsFragment()).commit();
+                break;
+            case R.id.navigation_account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AccountFragment()).commit();
+                break;
+            case R.id.navigation_logout:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 
 }
