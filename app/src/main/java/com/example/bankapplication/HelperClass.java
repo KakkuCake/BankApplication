90afaf90cd2ce7b147a17a8289afd04a6f126dba
vuorineuvosto <jopari.joona@gmail.com>
2020-05-06 17:15:42 +0300

    joo

--- app/src/main/java/com/example/bankapplication/HelperClass.java
+++ app/src/main/java/com/example/bankapplication/HelperClass.java
@@ -4,7 +4,8 @@ import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class HelperClass { //Luokka avustaa Database luokan getAccountBalance -metodia.
+public class HelperClass {
+    //Luokka avustaa Database luokan getAccountBalance -metodia.
     // Tarkoitus on tallentaa hetkellisesti Arraylist bListiin halutun käyttäjän balanssi, jotta osataan kirjoittaa tietokantaan uusi balanssi käyttäjälle.
 
     public float balance;
@@ -12,33 +13,31 @@ public class HelperClass { //Luokka avustaa Database luokan getAccountBalance -m
     protected static ArrayList<String> bList = new ArrayList<>();
     private static HelperClass b = null;
 
-   protected void saveBalance(String balance_string) {
+   protected void saveBalance(String balance_string) { // This ArrayList contains one balance, to help with things
        bList.add(balance_string);
-   } // Tämä
+   }
 
 
-    protected String getBalance() {
+    protected String getBalance() { //This method returns one saved balance
         for (String s : bList) {
             return s;
         }
         return null;
     }
 
-    public static HelperClass getInstance() {
+    public static HelperClass getInstance() { // we used singleton principle in this class
         if (b == null) {
             return new HelperClass();
         }
         return b;
     }
 
-    protected void clearList() {
+    protected void clearList() { // this method clears the one balance from the ArrayList
        bList.clear();
     }
-    protected void randomNumber(){
 
 
-    }
-    protected ArrayList<String> populateSpinner(String mEmail) {
+    protected ArrayList<String> populateSpinner(String mEmail) { //This method populates the spinner from which you choose the account type you want to create
 
        Bank bank = Bank.getInstance();
 
@@ -49,14 +48,14 @@ public class HelperClass { //Luokka avustaa Database luokan getAccountBalance -m
             System.out.println(a);
         }
 
-        nameList = new ArrayList(); //Täytetään ensiksi lista
+        nameList = new ArrayList();
         nameList.add("regularAccount");
         nameList.add("creditAccount");
         nameList.add("savingsAccount");
 
-        for (String s : arr) {   //Jos käyttäjällä on jo R-, C- tai S-tili niin poistetaan se pudotusvalikosta. Käyttäjällä saa olla max 1 tili/tilityyppi
+        for (String s : arr) {   //Account number's first letter determines the account type for ex. C1234567 is credit account.
             char first_letter = s.charAt(0);
-            String account_mark = "" + first_letter;  //Let's get the account mark (which is either R, C, or S) to know which account user is using.
+            String account_mark = "" + first_letter;
             if (account_mark.equals("R"))
                 nameList.remove("regularAccount");
             else if (account_mark.equals("C"))
@@ -69,7 +68,7 @@ public class HelperClass { //Luokka avustaa Database luokan getAccountBalance -m
 
     }
 
-    protected String getCardNumber(){
+    protected String getCardNumber(){ // When user creates a bankcard, this method generates the 16-digit number for it.
         String cardNumber = "";
         final Random random = new Random();
 
@@ -82,7 +81,5 @@ public class HelperClass { //Luokka avustaa Database luokan getAccountBalance -m
         }
         return cardNumber;
     }
-
-
 
 }
